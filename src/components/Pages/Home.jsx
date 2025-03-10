e6b2735eb30a41e56179bd3c4889317734876710
Ashik70021 <ashik70021@gmail.com>
2025-03-10 23:56:42 +0600

    setup environment

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import ProgressBar from "../ProgressBar";
+
+const Home = () => {
+    return (
+        <div>
+            <ProgressBar></ProgressBar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,11 @@
+const ProgressBar = () => {
+    return (
+        <div>
+            <h1>1. Characteristic 1</h1>
+            <h1>2. Characteristic 2</h1>
+            <h1>3. Characteristic 3</h1>
+        </div>
+    );
+};
+
+export default ProgressBar;
